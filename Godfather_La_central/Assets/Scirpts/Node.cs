743236dddb0a6b8227a2f687dd2367fc1f18d77d
clément bostyn <clemclem20002@gmail.com>
2023-09-08 13:42:56 +0200

    tkt

--- Godfather_La_central/Assets/Scirpts/Node.cs
+++ Godfather_La_central/Assets/Scirpts/Node.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEditor.Rendering;
-using UnityEditorInternal;
 using UnityEngine;
 using static Blackboard;
 using static System.Runtime.CompilerServices.RuntimeHelpers;
